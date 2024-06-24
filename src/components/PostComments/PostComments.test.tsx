9aaf128fb884852629cb000a2eab9ff9b8ca2cab
BaiacuPlays <andreibonatto8@gmail.com>
2024-06-24 14:39:21 -0300

    testes

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,21 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { render, screen, fireEvent } from '@testing-library/react';
+import PostComments from './index';
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
+test('inserts two comments', () => {
+  render(<PostComments />);
+
+  const input = screen.getByTestId('comment-input');
+  const button = screen.getByTestId('submit-button');
+
+  fireEvent.change(input, { target: { value: 'Primeiro comentario' } });
+  fireEvent.click(button);
+
+  fireEvent.change(input, { target: { value: 'Segundo comentario' } });
+  fireEvent.click(button);
+
+  const comments = screen.getAllByTestId('comment');
+
+  expect(comments).toHaveLength(2);
+  expect(comments[0]).toHaveTextContent('Primeiro comentario');
+  expect(comments[1]).toHaveTextContent('Segundo comentario');
 });
